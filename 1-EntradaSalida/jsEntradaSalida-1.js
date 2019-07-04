1714d0bf999c555d336e18345b7837559122aef8
DamianQuintana <52511412+DamianQuintana@users.noreply.github.com>
2019-07-03 21:26:55 -0300

    CLASE 1
    Ejercicios 
    1
    2
    3

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	alert("Esto funciona de maravilla"); //El archivo sobre el que queremos mostrar los cambios tiene que ser el mismo que tengamos abierto desde el compilador
+/*Bienvenidos. 
+Al presionar el botón, se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla".*/
+
 }
 
